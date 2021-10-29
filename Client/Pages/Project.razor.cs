f06f097192be6caa2c36d34667c2d76a9f9ed82c
TylerGoldenHart <90016672+TylerGoldenHart@users.noreply.github.com>
2021-10-28 23:31:07 -0400

    Update Project.razor.cs

--- Client/Pages/Project.razor.cs
+++ Client/Pages/Project.razor.cs
@@ -1,12 +1,20 @@
-public partial class Average
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Project.Pages
 {
-    public int num1 = 0;
-    public int num2 = 0;
-    public int num3 = 0;
-    public int avg = 0;
- 
-    public void DoAverage()
+
+    public partial class Average
     {
-        avg = (num1 + num2 + num3) / 3;
-    }
+        public int num1 = 0;
+        public int num2 = 0;
+        public int num3 = 0;
+        public int avg = 0;
+ 
+        public void DoAverage()
+        {
+            avg = (num1 + num2 + num3) / 3;
+        }
 }
