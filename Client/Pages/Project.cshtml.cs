70de3c1f91d2239927f27883efcfc89a7d22cd34
TylerGoldenHart <90016672+TylerGoldenHart@users.noreply.github.com>
2021-10-28 22:30:54 -0400

    Create Project.cshtml.cs

---
+++ Client/Pages/Project.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore.InMemory;
+
+namespace RazorPagesIntro.Pages {
+public class Project
+{
+    [Range(1, 99, ErrorMessage = "Number invalid.")]
+    public int num1 { get; set; }
+    
+    [Range(1, 99, ErrorMessage = "Number invalid.")]
+    public int num2 { get; set; }
+    
+    [Range(1, 99, ErrorMessage = "Number invalid.")]
+    public int num3 { get; set; }
+    
+    avg = (num1 + num2 + num3) / 3;
+}
+
+public class Index2Model : PageModel
+    {
+        public string Message { get; private set; } = "Null";
+
+        public void OnGet()
+        {
+            Message += $" Average is { avg }";
+        }
+    }
+}
