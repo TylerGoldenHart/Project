193a8e8a12dcc2f39a85683a5e29645718ca03f8
TylerGoldenHart <90016672+TylerGoldenHart@users.noreply.github.com>
2021-10-28 22:57:39 -0400

    Update and rename Project.cshtml.cs to Project.razor.cs

---
+++ Client/Pages/Project.razor.cs
@@ -0,0 +1,12 @@
+public partial class Average
+{
+    public int num1 = 0;
+    public int num2 = 0;
+    public int num3 = 0;
+    public int avg = 0;
+ 
+    public void DoAverage()
+    {
+        avg = (num1 + num2 + num3) / 3;
+    }
+}
